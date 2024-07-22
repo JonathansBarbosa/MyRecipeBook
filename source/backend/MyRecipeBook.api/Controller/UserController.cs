a336b89708fcf6cf835be069a4a565aa221b8ef0
Jonathan <dhoka680@gmail.com>
2024-07-22 19:21:16 -0300

    feat: adding endpoint to regsiter an user

---
+++ source/backend/MyRecipeBook.api/Controller/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Requests.Responses;
+
+namespace MyRecipeBook.api.Controller
+{
+    [Route("[controller]")] //Removido "api/" para não ficar repetindo 
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost] //método acionado quando um cliente envia uma requisição HTTP POST para o caminho associado ao endpoint.
+        [ProducesResponseType(typeof(ResponsesRegisterUserJson), StatusCodes.Status201Created)] //retorna no swagger 201 e não 200
+        public IActionResult Register(RequestRegisterUserJson request) // retorna nome,email,passowrld
+                                        //O tipo IActionResult representa o resultado de uma ação em um controlador
+        {
+            return Created(); //Cria a entidade usuario 
+        }
+    }
+}
---
+++ source/shared/MyRecipeBook.Communication/Requests/Responses/ResponsesRegisterUserJson.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Communication.Requests.Responses
+{
+    public class ResponsesRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
